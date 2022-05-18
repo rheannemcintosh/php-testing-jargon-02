c0b5dca1734ae5d42c049b019717dfd9acb9173b
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-05-18 17:24:04 +0100

    04 - When to Reach for Data Providers

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -13,43 +13,24 @@ class TagParserTest extends TestCase
         $this->parser = new TagParser();
     }
 
-    public function test_it_parses_a_single_tag()
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses_tags($input, $expected)
     {
-        $result = $this->parser->parse('personal');
-        $expected = ['personal'];
+        $result = $this->parser->parse($input);
 
         $this->assertSame($expected, $result);
     }
 
-    public function test_it_parses_a_comma_separated_list_of_tags_with_spaces()
+    public function tagsProvider()
     {
-        $result = $this->parser->parse('personal, money, family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
-    }
-
-    public function test_it_parses_a_comma_separated_list_of_tags_without_spaces()
-    {
-        $result = $this->parser->parse('personal,money,family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
-    }
-
-    public function test_it_parses_a_pipe_separated_list_of_tags_with_spaces()
-    {
-        $result = $this->parser->parse('personal | money | family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
-    }
-
-    public function test_it_parses_a_pipe_separated_list_of_tags_without_spaces()
-    {
-        $result = $this->parser->parse('personal|money|family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
+        return [
+            ['personal', ['personal']],
+            ['personal, money, family', ['personal', 'money', 'family']],
+            ['personal,money,family', ['personal', 'money', 'family']],
+            ['personal | money | family', ['personal', 'money', 'family']],
+            ['personal|money|family', ['personal', 'money', 'family']],
+        ];
     }
 }
